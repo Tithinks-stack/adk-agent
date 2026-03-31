b6ae4eccc3549751eac45ae6ea6623b7adf354f6
Tithi Gangar <tithivgangar07@gmail.com>
2026-03-31 18:37:23 +0530

    updated location agent

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+import os
 
 app = Flask(__name__)
 
@@ -15,6 +16,5 @@ def map_view():
     return "Google Maps integration simulated 🗺️"
 
 if __name__ == "__main__":
-    import os
     port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
